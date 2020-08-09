17618542ff6f073205f3b46d12927d4edad04d53
amit08255 <amitcute3@gmail.com>
2020-08-09 15:08:07 +0530

    Fixed message not sent to slack.

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-async function mainApp(req) {
-    // eslint-disable-next-line no-console
-    console.log(req, process.env.SLACK_WEBHOOK_URL);
+import axios from 'axios';
+import { text } from 'micro';
+
+// An utility function to be used with async function to handle promise (API calls) with await
+// without writing exception codes
+const to = (promise) => promise.then((data) => [null, data])
+    .catch((err) => [err]);
+
+async function mainApp(req, res) {
+    const body = await text(req);
+    const data = { text: body };
+
+    await to(axios.post(process.env.SLACK_WEBHOOK_URL, data));
+
+    res.send('Welcome to Slack-Dev Bot!');
 }
 
-module.exports = mainApp;
+export default mainApp;
